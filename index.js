f98399699605937e7e8ea7cfcb00170a3e769eeb
Bharat Nagda <nagdabharat@gmail.com>
2020-06-09 12:34:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 Function updateObjectWithKeyAndValue(object, key, value)  {
-  //return object;
   var newobject = Object.assign({},object)
   Object.assign(newobject,{[key]:value});
   return newobject;
