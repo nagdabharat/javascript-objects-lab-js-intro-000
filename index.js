360fcc46248f59813373605b9e12d11faf474852
Bharat Nagda <nagdabharat@gmail.com>
2020-06-09 12:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var object = {};
+//var object = {};
 function updateObjectWithKeyAndValue(object, key, value)  {
   var newobject = Object.assign({},object)
   Object.assign(newobject,{[key]:value});
